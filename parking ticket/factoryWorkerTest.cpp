172b8c92e393bf06149a968cac44a74d3ede7fef
omnipelican <loganjakus@icloud.com>
2024-11-15 23:13:06 -0500

    did things

--- parking ticket/factoryWorkerTest.cpp
+++ parking ticket/factoryWorkerTest.cpp
@@ -11,13 +11,13 @@ int main()
      
     testPW1.setEmpName("Paul Jones"); 
     testPW1.setEmpNum(12345);
-    testPW1.setHireDate(10, 28, 2024); 
+    testPW1.setEmpHireDate(10, 28, 2024); 
     testPW1.setShift(1);
     testPW1.setPayRate(20.25);
 
     testPW2.setEmpName("Harriet Smith"); 
     testPW2.setEmpNum(54321);  
-    testPW2.setHireDate(6, 15, 2024);  
+    testPW2.setEmpHireDate(6, 15, 2024);  
     testPW2.setShift(2); 
     testPW2.setPayRate(25.50);  
 
